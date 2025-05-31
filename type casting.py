c847aa8f0cce098e95d335decde2c1a84dcf9c9a
lunar_eclipse <sleepysmorz@gmail.com>
2025-05-31 20:38:11 +1200

    type casting

---
+++ type casting.py
@@ -0,0 +1,19 @@
+name = "chid"
+age = 743
+is_alien = True
+weight = 23.4
+print("name:", name)
+print("data type of name is", type(name))
+print("age:", age)
+print("data type of age is", type(age))
+print("is_alien :", is_alien)
+print("data type of is_alien is", type(is_alien))
+print("weight :", weight)
+print("data type of weight is", type(weight))
+print("\n after type casting....")
+age = str(age)
+print(age)
+print("data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is", type(weight))
