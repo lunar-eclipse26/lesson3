46317a547ba682538eef9db7ad92ae8b878df674
lunar_eclipse <sleepysmorz@gmail.com>
2025-06-04 18:17:45 +1200

    lesson 3 acp

---
+++ lesson 3 acp.py
@@ -0,0 +1,8 @@
+e="TREE"
+a="a chid is ancient race of space aliens"
+b="congratulations on winning the lottery"
+c="when the sun explodes a planet will "
+print(e.lower())
+print(a.split())
+print(b.upper())
+print(c+e)
